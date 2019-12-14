883c90473b67186bff4dffc3f2b409b9bf9982c4
danielzmod <daniel.zmod@gmail.com>
2019-12-14 16:59:09 +0100

    First try. Doesnt work

---
+++ 1_install_pyenv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Source https://github.com/pyenv/pyenv
+# Installer https://github.com/pyenv/pyenv-installer
+# From installation wiki following packages are needed
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+# Run installation script
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
